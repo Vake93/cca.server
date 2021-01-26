80c797d8a4ab23dc7b88044ec857c177e36f5174
Vishvaka Ranasinghe <vishvaka93@live.com>
2021-01-26 17:25:36 +0530

    Added Hello World Service

---
+++ CCA.User.Service/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using CCA.Application.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace CCA.User.Service
+{
+    public static class HelloWorld
+    {
+        [FunctionName("hello-world")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, HttpMethod.GET, Route = "hello-world")] HttpRequest request)
+        {
+            return new OkObjectResult("Hello, World");
+        }
+    }
+}
